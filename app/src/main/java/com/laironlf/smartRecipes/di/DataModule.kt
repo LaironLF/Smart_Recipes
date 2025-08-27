61cf55af41ba36a6adfd74c66e3e0d54b39e5a62
laironlf <laironlf@gmail.com>
2025-08-27 12:43:25 +0700

    new api ip

--- app/src/main/java/com/laironlf/smartRecipes/di/DataModule.kt
+++ app/src/main/java/com/laironlf/smartRecipes/di/DataModule.kt
@@ -3,11 +3,8 @@ package com.laironlf.smartRecipes.di
 
 import android.content.Context
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.laironlf.smartRecipes.data.api.BarcodeApiService
-import com.laironlf.smartRecipes.data.api.GS1RusApiService
 import com.laironlf.smartRecipes.data.api.RecipesApiService
 import com.laironlf.smartRecipes.data.cache.AppCaching
-import com.laironlf.smartRecipes.data.repository.BarcodeRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,10 +12,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -31,15 +25,11 @@ class DataModule {
 
     }
 
-    private val client = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().apply { this.setLevel(HttpLoggingInterceptor.Level.BODY) })
-        .build()
-
     @Provides
     @Singleton
     fun provideSmartRecipesApi() : RecipesApiService {
         return Retrofit.Builder()
-            .baseUrl("http://185.221.214.178:9999/")
+            .baseUrl("http://45.43.91.214:9999/")
             .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
             .create(RecipesApiService::class.java)
@@ -50,10 +40,4 @@ class DataModule {
     fun provideAppCachig(@ApplicationContext context: Context): AppCaching = AppCaching(context)
 
 
-
-//    @Provides
-//    @Singleton
-//    fun provideRoomDB(){
-//
-//    }
 }
